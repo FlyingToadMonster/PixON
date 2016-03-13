61530258a7353c54b890ef6ed3348da499cce3d1
HydroH <2322264211@qq.com>
2016-03-13 14:55:55 +0800

    Basic database frame, basic GUI

--- PixON/PixON.Designer.cs
+++ PixON/PixON.Designer.cs
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbPathList = new System.Windows.Forms.ListBox();
+            this.tbPathInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.fbdSelectPath = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
+            // 
+            // lbPathList
+            // 
+            this.lbPathList.FormattingEnabled = true;
+            this.lbPathList.ItemHeight = 12;
+            this.lbPathList.Location = new System.Drawing.Point(23, 68);
+            this.lbPathList.Name = "lbPathList";
+            this.lbPathList.Size = new System.Drawing.Size(179, 136);
+            this.lbPathList.TabIndex = 0;
+            // 
+            // tbPathInput
+            // 
+            this.tbPathInput.Location = new System.Drawing.Point(23, 41);
+            this.tbPathInput.Name = "tbPathInput";
+            this.tbPathInput.Size = new System.Drawing.Size(135, 21);
+            this.tbPathInput.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(217, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "添加路径";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(217, 68);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "删除选定项";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(164, 41);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(38, 23);
+            this.btnSelectPath.TabIndex = 4;
+            this.btnSelectPath.Text = "...";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
             // 
             // PixON
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(314, 247);
+            this.Controls.Add(this.btnSelectPath);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbPathInput);
+            this.Controls.Add(this.lbPathList);
             this.Name = "PixON";
             this.Text = "PixON";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbPathList;
+        private System.Windows.Forms.TextBox tbPathInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSelectPath;
+        private System.Windows.Forms.FolderBrowserDialog fbdSelectPath;
     }
 }
 
---
+++ PixON/PixONDb.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PixON
+{
+    class PixONDb
+    {
+        private static SQLiteConnection pixDbConn;
+
+        private string GetMD5(string fileName)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    return Encoding.Default.GetString(md5.ComputeHash(stream));
+                }
+            }
+        }
+
+        public PixONDb(string pixDbName = "db/pixDb.sqlite")
+        {
+            if (!File.Exists(pixDbName))
+            {
+                string pixDbPath = Path.GetDirectoryName(pixDbName);
+                Directory.CreateDirectory(pixDbPath);
+                SQLiteConnection.CreateFile(pixDbName);
+            }
+            pixDbConn = new SQLiteConnection("Data Source=" + pixDbName);
+            pixDbConn.Open();
+
+            SQLiteCommand cmd = new SQLiteCommand(@"
+CREATE TABLE IF NOT EXISTS FileDb 
+(
+hash char(32) NOT NULL, 
+path varchar(260) NOT NULL, 
+CONSTRAINT hash PRIMARY KEY (hash), 
+UNIQUE (path)
+)"
+                , pixDbConn);
+            cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand(@"
+CREATE TABLE IF NOT EXISTS InfoDb
+(
+id int NOT NULL AUTO_INCREMENT, 
+hash char(32) NOT NULL, 
+key varchar(255) NOT NULL, 
+value varchar(8000), 
+PRIMARY KEY (id), 
+INDEX (hash)
+)"
+                , pixDbConn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public string AddFile(string fileName)
+        {
+            string md5 = GetMD5(fileName);
+            SQLiteCommand cmd = new SQLiteCommand("INSERT INTO FileDb (hash, path) VALUES (@Hash, @Path) ON DUPLICATE KEY UPDATE path=VALUES(path)", pixDbConn);
+            cmd.Parameters.AddWithValue("Hash", md5);
+            cmd.Parameters.AddWithValue("Path", fileName);
+            cmd.ExecuteNonQuery();
+            return md5;
+        }
+
+        public void AddProp(string hash, string key, string value)
+        {
+            SQLiteCommand cmd = new SQLiteCommand("SELECT COUNT(*) FROM InfoDb WHERE hash=@Hash AND key=@Key AND value=@VALUE", pixDbConn);
+            cmd.Parameters.AddWithValue("Hash", hash);
+            cmd.Parameters.AddWithValue("Key", key);
+            cmd.Parameters.AddWithValue("Value", value);
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            rdr.Read();
+            if (rdr.GetInt32(0) == 0)
+            {
+                cmd = new SQLiteCommand("INSERT INTO InfoDB (hash, key, value) VALUES (@Hash, @Key, @Value)", pixDbConn);
+                cmd.Parameters.AddWithValue("Hash", hash);
+                cmd.Parameters.AddWithValue("Key", key);
+                cmd.Parameters.AddWithValue("Value", value);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<string> GetProp(string hash, string key)
+        {
+            List<string> result = new List<string>();
+            SQLiteCommand cmd = new SQLiteCommand("SELECT value FROM InfoDb WHERE hash=@Hash AND key=@Key", pixDbConn);
+            cmd.Parameters.AddWithValue("Hash", hash);
+            cmd.Parameters.AddWithValue("Key", key);
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                result.Add(rdr.GetString(0));
+            }
+            return result;
+        }
+
+        public void Close()
+        {
+            pixDbConn.Close();
+        }
+    }
+}
